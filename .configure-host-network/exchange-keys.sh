dc082b5fd5622e0c8f694fec3bb66f11884c08c8
Filippo Gurioli <gurfi2001@gmail.com>
2026-09-04 19:45:50 +0200

    fix: use root instead of rioly user

--- .configure-host-network/exchange-keys.sh
+++ .configure-host-network/exchange-keys.sh
@@ -2,5 +2,5 @@
 
 ssh-copy-id -i ~/.ssh/homelab-proxmox.pub root@homelab.lan
 ssh-copy-id -i ~/.ssh/homelab-gateway.pub root@gateway.homelab.lan
-ssh-copy-id -i ~/.ssh/homelab-backup.pub rioly@backup.homelab.lan
-ssh-copy-id -i ~/.ssh/homelab-application.pub rioly@application.homelab.lan
+ssh-copy-id -i ~/.ssh/homelab-backup.pub root@backup.homelab.lan
+ssh-copy-id -i ~/.ssh/homelab-application.pub root@application.homelab.lan
