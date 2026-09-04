bf4ca57de72979017c42765db04f13b60425416f
Filippo Gurioli <gurfi2001@gmail.com>
2026-09-04 21:02:04 +0200

    revert: return to rioly account

--- .configure-host-network/exchange-keys.sh
+++ .configure-host-network/exchange-keys.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-ssh-copy-id -i ~/.ssh/homelab-proxmox.pub root@homelab.lan
-ssh-copy-id -i ~/.ssh/homelab-gateway.pub root@gateway.homelab.lan
-ssh-copy-id -i ~/.ssh/homelab-backup.pub root@backup.homelab.lan
-ssh-copy-id -i ~/.ssh/homelab-application.pub root@application.homelab.lan
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [ -f "$SCRIPT_DIR/.env" ]; then
+  set -o allexport
+  source "$SCRIPT_DIR/.env"
+  set +o allexport
+fi
+
+sshpass -p "$PVE_PSW" ssh-copy-id -i ~/.ssh/homelab-proxmox.pub root@homelab.lan
+sshpass -p "$GTW_PSW" ssh-copy-id -i ~/.ssh/homelab-gateway.pub root@gateway.homelab.lan
+sshpass -p "$BKU_PSW" ssh-copy-id -i ~/.ssh/homelab-backup.pub rioly@backup.homelab.lan
+sshpass -p "$APP_PSW" ssh-copy-id -i ~/.ssh/homelab-application.pub rioly@application.homelab.lan
